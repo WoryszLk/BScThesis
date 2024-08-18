a13ae5f07b2efe08a7260a2fe935f86b0191cb5a
Lukasz <woryszlukasz@gmail.com>
2024-08-18 15:00:55 +0200

    Adding new launch setting

---
+++ application_supporting_the_management_of_shooting_competitions/lib/pages/competition_start_page.dart
@@ -0,0 +1,82 @@
+import 'package:application_supporting_the_management_of_shooting_competitions/pages/player_list_selector_page.dart';
+import 'package:flutter/material.dart';
+import 'competition_selector_page.dart';
+import 'package:application_supporting_the_management_of_shooting_competitions/components/custom_button.dart';
+
+class StarterCompetition extends StatelessWidget {
+  const StarterCompetition({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('App'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            // Lewa kolumna (duży kafelek)
+            Expanded(
+              flex: 2,
+              child: CustomButton(
+                width: double.infinity,
+                height: double.infinity,
+                imagePath: 'lib/images/buttonShooters.jpg',
+                text: 'Wybierz typ zawodów',
+                onPressed: () {
+                  Navigator.push(context, 
+                  MaterialPageRoute(builder: (context) => const CompetitionSelector()));
+                },
+              ),
+            ),
+            const SizedBox(width: 8), // Przerwa między kolumnami
+            // Prawa kolumna (trzy mniejsze kafelki)
+            Expanded(
+              flex: 1,
+              child: Column(
+                children: [
+                  // Górny kafelek
+                  Expanded(
+                    child: CustomButton(
+                      width: double.infinity,
+                      height: double.infinity,
+                      imagePath: 'lib/images/PlayersList.jpg',
+                      text: 'Lista zawodników',
+                      onPressed: () {
+                        Navigator.push(context, 
+                        MaterialPageRoute(builder: (context) => const PlayerList()));
+                      },
+                    ),
+                  ),
+                  const SizedBox(height: 8), // Przerwa między kafelkami
+                  // Środkowy kafelek
+                  Expanded(
+                    child: CustomButton(
+                      width: double.infinity,
+                      height: double.infinity,
+                      imagePath: 'lib/images/buttonShooters.jpg',
+                      text: 'Tile 2',
+                      onPressed: () {},
+                    ),
+                  ),
+                  const SizedBox(height: 8), // Przerwa między kafelkami
+                  // Dolny kafelek
+                  Expanded(
+                    child: CustomButton(
+                      width: double.infinity,
+                      height: double.infinity,
+                      imagePath: 'lib/images/buttonShooters.jpg',
+                      text: 'Tile 3',
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- application_supporting_the_management_of_shooting_competitions/lib/pages/home_page.dart
+++ application_supporting_the_management_of_shooting_competitions/lib/pages/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:application_supporting_the_management_of_shooting_competitions/pages/player_list_selector_page.dart';
 import 'package:flutter/material.dart';
-import 'competition_selector_page.dart';
+import 'competition_start_page.dart';
 import 'package:application_supporting_the_management_of_shooting_competitions/components/custom_button.dart';
 
 class HomePage extends StatelessWidget {
@@ -14,61 +14,66 @@ class HomePage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Row(
+        child: Column(
           children: [
-            // Lewa kolumna (duży kafelek)
-            Expanded(
-              flex: 2,
-              child: CustomButton(
-                width: double.infinity,
-                height: double.infinity,
-                imagePath: 'lib/images/buttonShooters.jpg',
-                text: 'Wybierz typ zawodów',
-                onPressed: () {
-                  Navigator.push(context, 
-                  MaterialPageRoute(builder: (context) => const CompetitionSelector()));
-                },
-              ),
+            // Górny button (Wybierz typ zawodów)
+            CustomButton(
+              width: double.infinity,
+              height: 450, // Możesz dostosować wysokość według potrzeb
+              imagePath: 'lib/images/ZacznijZawody.jpg',
+              text: 'Rozpocznij zawody',
+              onPressed: () {
+                Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const StarterCompetition()));
+              },
             ),
-            const SizedBox(width: 8), // Przerwa między kolumnami
-            // Prawa kolumna (trzy mniejsze kafelki)
+            const SizedBox(height: 8), // Przerwa między rzędem górnym a dolnym
+            // Dolny rząd z dwoma kolumnami
             Expanded(
-              flex: 1,
-              child: Column(
+              child: Row(
                 children: [
-                  // Górny kafelek
+                  // Lewa kolumna (Lista zawodników)
                   Expanded(
+                    flex: 2,
                     child: CustomButton(
                       width: double.infinity,
                       height: double.infinity,
                       imagePath: 'lib/images/PlayersList.jpg',
-                      text: 'Lista zawodników',
+                      text: 'Moje zawody',
                       onPressed: () {
-                        Navigator.push(context, 
+                        Navigator.push(context,
                         MaterialPageRoute(builder: (context) => const PlayerList()));
                       },
                     ),
                   ),
-                  const SizedBox(height: 8), // Przerwa między kafelkami
-                  // Środkowy kafelek
+                  const SizedBox(width: 8), // Przerwa między kolumnami
+                  // Prawa kolumna z dwoma kafelkami (Tile 3 i Tile 4)
                   Expanded(
-                    child: CustomButton(
-                      width: double.infinity,
-                      height: double.infinity,
-                      imagePath: 'lib/images/buttonShooters.jpg',
-                      text: 'Tile 2',
-                      onPressed: () {},
-                    ),
-                  ),
-                  const SizedBox(height: 8), // Przerwa między kafelkami
-                  // Dolny kafelek
-                  Expanded(
-                    child: CustomButton(
-                      width: double.infinity,
-                      height: double.infinity,
-                      imagePath: 'lib/images/buttonShooters.jpg',
-                      text: 'Tile 3',
-                      onPressed: () {},
+                    flex: 1,
+                    child: Column(
+                      children: [
+                        // Tile 3 (górny kafelek)
+                        Expanded(
+                          child: CustomButton(
+                            width: double.infinity,
+                            height: double.infinity,
+                            imagePath: 'lib/images/buttonShooters.jpg',
+                            text: 'Tile 3',
+                            onPressed: () {},
+                          ),
+                        ),
+                        const SizedBox(height: 8), // Przerwa między kafelkami
+                        // Tile 4 (dolny kafelek)
+                        Expanded(
+                          child: CustomButton(
+                            width: double.infinity,
+                            height: double.infinity,
+                            imagePath: 'lib/images/buttonShooters.jpg',
+                            text: 'Tile 4',
+                            onPressed: () {},
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ],
