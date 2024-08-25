da8f5c0340e71697ad09d14c6ec7547a6965d64c
Lukasz <woryszlukasz@gmail.com>
2024-08-25 20:33:46 +0200

    Addingfirestore and user_repo config

--- application_supporting_the_management_of_shooting_competitions/packages/user_repository/lib/src/firebase_user_repo.dart
+++ application_supporting_the_management_of_shooting_competitions/packages/user_repository/lib/src/firebase_user_repo.dart
@@ -1,28 +1,31 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:user_repository/src/models/user.dart';
 import 'package:user_repository/src/user_repo.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class firebaseUserRepo implements UserRepository{
+  final FirebaseAuth _firebaseAuth;
+  final usersCollection = FirebaseFirestore.instance.collection('user');
+
+  firebaseUserRepo({
+    FirebaseAuth? firebaseAuth,
+  }) : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
+
   @override
-  Future<void> setUserData(MyUser user) {
-    // TODO: implement setUserData
-    throw UnimplementedError();
-  }
+  Stream<User?> get user => throw UnimplementedError();
 
   @override
   Future<void> signIn(String email, String password) {
-    // TODO: implement signIn
     throw UnimplementedError();
   }
 
   @override
   Future<MyUser> signUp(MyUser myUser, String password) {
-    // TODO: implement signUp
     throw UnimplementedError();
   }
 
-  @override
-  // TODO: implement user
-  Stream<User?> get user => throw UnimplementedError();
-
+   @override
+  Future<void> setUserData(MyUser user) {
+    throw UnimplementedError();
+  }
 }
