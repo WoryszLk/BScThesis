a27d61637921ebf897941717a02d05954073a064
Lukasz <woryszlukasz@gmail.com>
2024-08-25 16:10:25 +0200

    creating empty method

--- application_supporting_the_management_of_shooting_competitions/packages/user_repository/lib/src/models/user.dart
+++ application_supporting_the_management_of_shooting_competitions/packages/user_repository/lib/src/models/user.dart
@@ -10,6 +10,24 @@ class MyUser extends Equatable {
     required this.name,
   });
   
+  static const empty = MyUser(
+    userId: '', 
+    email: '', 
+    name: ''
+    );
+
+  MyUser copyWith({
+    String? userId,
+    String? email,
+    String? name
+  }) {
+    return MyUser(
+      userId: userId ?? this.userId, 
+      email: email ?? this.email, 
+      name: name ?? this.name
+      );
+  }
+
   @override
   List<Object?> get props => [userId, email, name];
 }
