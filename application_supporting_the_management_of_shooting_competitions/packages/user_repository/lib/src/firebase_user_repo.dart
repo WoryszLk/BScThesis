3330cac305d153637f39e72a8e6b62a09e743a6e
Lukasz <woryszlukasz@gmail.com>
2024-08-25 19:30:34 +0200

    adding firebase authenthication

---
+++ application_supporting_the_management_of_shooting_competitions/packages/user_repository/lib/src/firebase_user_repo.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:user_repository/src/models/user.dart';
+import 'package:user_repository/src/user_repo.dart';
+
+class firebaseUserRepo implements UserRepository{
+  @override
+  Future<void> setUserData(MyUser user) {
+    // TODO: implement setUserData
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signIn(String email, String password) {
+    // TODO: implement signIn
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<MyUser> signUp(MyUser myUser, String password) {
+    // TODO: implement signUp
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement user
+  Stream<User?> get user => throw UnimplementedError();
+
+}
