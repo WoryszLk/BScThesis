219bb78e867f3acf685f52cdfa1d6cb5111b251e
Lukasz <woryszlukasz@gmail.com>
2024-08-31 16:06:40 +0200

    adding auth states and events

--- application_supporting_the_management_of_shooting_competitions/lib/blocs/authentication_bloc/authentication_bloc.dart
+++ application_supporting_the_management_of_shooting_competitions/lib/blocs/authentication_bloc/authentication_bloc.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 part 'authentication_event.dart';
 part 'authentication_state.dart';
