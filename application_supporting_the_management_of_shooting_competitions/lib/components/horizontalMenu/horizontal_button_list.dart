d412ccc6980e6ff86ccff787cd996bd57806355a
Lukasz <woryszlukasz@gmail.com>
2024-09-16 22:03:15 +0200

    modifying horizontal list button

--- application_supporting_the_management_of_shooting_competitions/lib/components/horizontalMenu/horizontal_button_list.dart
+++ application_supporting_the_management_of_shooting_competitions/lib/components/horizontalMenu/horizontal_button_list.dart
@@ -1,3 +1,4 @@
+import 'package:application_supporting_the_management_of_shooting_competitions/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 class HorizontalButtonList extends StatelessWidget {
@@ -9,58 +10,58 @@ class HorizontalButtonList extends StatelessWidget {
       scrollDirection: Axis.horizontal,
       child: Row(
         children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: ElevatedButton(
-              onPressed: () {
-              },
-              child: const Text('Test 1'),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: ElevatedButton(
-              onPressed: () {
-              },
-              child: const Text('Test 2'),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: ElevatedButton(
-              onPressed: () {
-              },
-              child: const Text('Test 3'),
-            ),
-          ),
+          // testingna ten moment
+          _buildCustomButton(context, 'Test 1', 'lib/images/PlayersList.jpg', HomePage()),
+          _buildCustomButton(context, 'Test 1', 'lib/images/PlayersList.jpg', HomePage()),
+          _buildCustomButton(context, 'Test 1', 'lib/images/PlayersList.jpg', HomePage()),
+          _buildCustomButton(context, 'Test 1', 'lib/images/PlayersList.jpg', HomePage()),
+          _buildCustomButton(context, 'Test 1', 'lib/images/PlayersList.jpg', HomePage()),
+          _buildCustomButton(context, 'Test 1', 'lib/images/PlayersList.jpg', HomePage()),
 
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: ElevatedButton(
-              onPressed: () {
-              },
-              child: const Text('Test 4'),
-            ),
-          ),
-
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: ElevatedButton(
-              onPressed: () {
-              },
-              child: const Text('Test 5'),
-            ),
-          ),
-
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: ElevatedButton(
-              onPressed: () {
-              },
-              child: const Text('Test 6'),
-            ),
-          ),
         ],
+      ),
+    );
+  }
+
+  Widget _buildCustomButton(BuildContext context, String label, String imagePath, Widget routePath) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => routePath),
+          );
+        },
+        child: Container(
+          width: 120,
+          height: 120,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16.0),
+            image: DecorationImage(
+              image: AssetImage(imagePath),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Center(
+            child: Text(
+              label,
+              style: const TextStyle(
+                color: Colors.white, 
+                fontSize: 16, 
+                fontWeight: FontWeight.bold,
+                shadows: [
+                  Shadow(
+                    offset: Offset(0, 1),
+                    blurRadius: 3.0,
+                    color: Colors.black54,
+                  ),
+                ],
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
       ),
     );
   }
